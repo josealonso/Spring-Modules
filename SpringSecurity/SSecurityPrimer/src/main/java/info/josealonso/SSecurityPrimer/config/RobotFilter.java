4d667c16dfb1598f3e2b080ed62b2f2d84a5b85a
josealonso <jose.alonso.programmer@gmail.com>
2023-01-10 00:13:27 +0100

    Spring Security: implemented a custom filter for Mr. Robot.

--- SpringSecurity/SSecurityPrimer/src/main/java/info/josealonso/SSecurityPrimer/config/RobotFilter.java
+++ SpringSecurity/SSecurityPrimer/src/main/java/info/josealonso/SSecurityPrimer/config/RobotFilter.java
@@ -1,5 +1,7 @@
 package info.josealonso.SSecurityPrimer.config;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,7 +18,23 @@ public class RobotFilter extends OncePerRequestFilter {
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
-        System.out.println("Hello from the ROBOT FILTER");
+        // 1. Authentication decision
+        var password = request.getHeader("x-robot-password");
+        if (!"beep-boop".equals(password)) {
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setCharacterEncoding("utf-8");
+            response.setHeader("Content-type", "text/plain;charset+utf-8");
+            response.getWriter().println("==== You are not Mr Robot====");
+            return;
+        }
+
+        var newContent = SecurityContextHolder.createEmptyContext();
+        newContent.setAuthentication(new RobotAuthentication());
+        SecurityContextHolder.setContext(newContent);
         filterChain.doFilter(request, response);
+        return;
+
+        // 2.- Do the rest
+
     }
 }
