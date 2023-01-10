47221cccc6373145e168d0511114f5761847deea
josealonso <jose.alonso.programmer@gmail.com>
2023-01-10 17:51:22 +0100

    Spring Security: corrected the robot filter.

--- SpringSecurity/SSecurityPrimer/src/main/java/info/josealonso/SSecurityPrimer/config/RobotFilter.java
+++ SpringSecurity/SSecurityPrimer/src/main/java/info/josealonso/SSecurityPrimer/config/RobotFilter.java
@@ -10,16 +10,26 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 
 // public class RobotFilter extends GenericFilterBean {
 public class RobotFilter extends OncePerRequestFilter {
+
+    public static final String HEADER_NAME = "x-robot-password";
+
     @Override
     protected void doFilterInternal(
             HttpServletRequest request,
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
+        // 0. Should execute filter ?
+        if (!Collections.list(request.getHeaderNames()).contains(HEADER_NAME)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
         // 1. Authentication decision
-        var password = request.getHeader("x-robot-password");
+        var password = request.getHeader(HEADER_NAME);
         if (!"beep-boop".equals(password)) {
             response.setStatus(HttpStatus.FORBIDDEN.value());
             response.setCharacterEncoding("utf-8");
